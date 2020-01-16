634fc542ee227f93eb0b6e1e871949d6f9e64683
Krzysztof Woliński <bodziowagh@gmail.com>
2020-01-16 17:43:46 +0900

    Added tests for the happy path

--- src/robot/robot.spec.js
+++ src/robot/robot.spec.js
@@ -3,7 +3,7 @@ const robot = require(".");
 describe("Robot", () => {
     it("should fail on providing an empty config", () => {
         expect(robot.clean).toThrow();
-    })
+    });
 
     it("should fail on providing an invalid config", () => {
         const invalidConfigs = [
@@ -18,5 +18,42 @@ describe("Robot", () => {
         for (invalidConfig of invalidConfigs) {
             expect(() => robot.clean(invalidConfig)).toThrow();
         }
-    })
+    });
+
+    it("should return a cleaning report for a simple config", () => {
+        const config = {
+            x: 0,
+            y: 0,
+            stepsCount: 1,
+            steps: [{
+                direction: "E",
+                distance: 1
+            }]
+        };
+
+        expect(robot.clean(config)).toEqual("=> Cleaned: 2");
+    });
+
+    it("should include only unique places in the cleaning report", () => {
+        const config = {
+            x: 0,
+            y: 0,
+            stepsCount: 4,
+            steps: [{
+                direction: "N",
+                distance: 1
+            }, {
+                direction: "S",
+                distance: 1
+            }, {
+                direction: "N",
+                distance: 1
+            }, {
+                direction: "S",
+                distance: 1
+            }]
+        };
+
+        expect(robot.clean(config)).toEqual("=> Cleaned: 2");
+    });
 });
