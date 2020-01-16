29db18fbdae59ee64eb261ea90858ea4c5250e8e
Krzysztof Woliński <bodziowagh@gmail.com>
2020-01-16 17:09:35 +0900

    Validating config

--- src/robot/index.js
+++ src/robot/index.js
@@ -1,5 +1,29 @@
+const _ = require("lodash");
+
+/*
+ *  config:       
+ *      - x: number
+ *      - y: number
+ *      - stepsCount: number
+ *      - steps: Array<{
+ *           direction: 'N' | 'E' | 'S' | 'W'
+ *           distance: number
+ *        }>
+ */
+
+const isConfigValid = ({ x, y, stepsCount, steps }) => 
+    (_.isNil(x) || _.isNil(y) || stepsCount !== steps.length) ? false : true;
+
 module.exports = {
     clean: (config) => {
+        if (!config) {
+            throw Error("Config has not been provided!");
+        }
+
+        if (!isConfigValid(config)) {
+            throw Error("Config is invalid!");
+        }
+
         const placesCount = 0;
 
         return `=> Cleaned: ${placesCount}`
--- src/robot/robot.spec.js
+++ src/robot/robot.spec.js
@@ -1,7 +1,22 @@
 const robot = require(".");
 
 describe("Robot", () => {
-    it("should return a formatted report from clean method", () => {
-        expect(robot.clean()).toEqual("=> Cleaned: 0");
+    it("should fail on providing an empty config", () => {
+        expect(robot.clean).toThrow();
+    })
+
+    it("should fail on providing an invalid config", () => {
+        const invalidConfigs = [
+            {}, {
+                x: 0,
+                y: 0,
+                steps: [],
+                stepsCount: 1
+            }
+        ]
+        
+        for (invalidConfig of invalidConfigs) {
+            expect(() => robot.clean(invalidConfig)).toThrow();
+        }
     })
 });
